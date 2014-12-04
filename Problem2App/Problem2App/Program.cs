a7ad2abd1102bd47d990873b4a4e918719c36f50
nrifath2009 <nrifath2009@gmail.com>
2014-12-05 01:00:20 +0600

    Problem Set 2 Solution

---
+++ Problem2App/Problem2App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 sum = 0;
+            Int64 num1 = 0;
+            Int64 num2 = 1;
+            Int64 result = 0;
+
+            while (result<4000000)
+            {
+                result = num1 + num2;
+                num1 = num2;
+                num2 = result;
+                if (num2 % 2 == 0)
+                {
+                    sum += num2;
+                    Console.WriteLine(num2);
+                }
+                
+            }
+
+            Console.WriteLine("Sum is: "+sum);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Problem6App/Problem6App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumOfSquare = 0;
+            double squareOfSum = 0;
+            int sumOfHundredNaturalNumbers = 0;
+            double difference = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sumOfSquare += Math.Pow(i, 2);
+                sumOfHundredNaturalNumbers += i;
+            }
+            squareOfSum = Math.Pow(sumOfHundredNaturalNumbers, 2);
+            difference = squareOfSum - sumOfSquare;
+            Console.WriteLine(difference);
+            Console.ReadKey();
+        }
+    }
+}
